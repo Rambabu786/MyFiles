fb1165786d821d36f59fb2706490addfd293cf5e
Rambabu786 <rambabu4unix@gmail.com>
2017-10-25 22:30:03 +0530

    Updated Package attributes in the attributs file

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+case platform 
+when "redhat","centos","fedora","scientific" 
+default[:ntp][:service] = "ntpd" 
+when "ubuntu","debian" 
+default[:ntp][:service] = "ntp" 
+else 
+default[:ntp][:service] = "ntpd" 
+end
