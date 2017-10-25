f317165252387d92af6edb9f926bbf51e6041bf7
Rambabu786 <rambabu4unix@gmail.com>
2017-10-25 22:19:19 +0530

    added package resource to my recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package node[:ntp][:service] do 
+action :install
+end
+template "/etc/ntp.conf" do 
+source "ntp.conf.erb" 
+variables( :ntp_server => "time.nist.gov" ) 
+notifies :restart, "service[ntpd]" 
+end
+service node[:ntp][:service] do 
+service_name node[:ntp][:service] 
+action [:enable,:start,:restart] 
+end
